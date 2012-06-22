11bd15b14a96140951970550cec26f7b4c5a9b3b
Zahir Koradia <zahir.koradia@gmail.com>
2012-06-22 14:29:02 +0530

    Further changes to improve installation process

--- VoiceApplications/VoiceApplications/settings.py
+++ VoiceApplications/VoiceApplications/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'PhonePeti',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'piyush27',                  # Not used with sqlite3.
+        'PASSWORD': 'junoon',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -105,7 +105,7 @@ ROOT_URLCONF = 'VoiceApplications.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'VoiceApplications.wsgi.application'
 
-TEMPLATE_DIRS = ( '/home/piyush/Documents/Django/Projects/VoiceApplications/Templates'
+TEMPLATE_DIRS = ( '/home/zahirk/PhonePeti/VoiceApplications/Templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
