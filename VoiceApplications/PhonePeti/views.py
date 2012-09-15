eb8538e770fdb1a93800c1cb85eabe672e04e43e
Zahir Koradia <zahir.koradia@gmail.com>
2012-09-15 10:00:12 +0530

    added pagination in feedback app

--- VoiceApplications/PhonePeti/views.py
+++ VoiceApplications/PhonePeti/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
 from Feedback.models import Response
 from PhonePeti.models import Call
+from django.core.paginator import *
 
 def index(request):
 	return render_to_response('index.html', {'user': request.user})
@@ -14,8 +15,16 @@ def logout_page(request):
 	return HttpResponseRedirect('/')
 
 def feedbacks(request):
-	all_feedbacks = Response.objects.all().order_by('-id')
-	return render_to_response('feedbacks.html', {'all_feedbacks': all_feedbacks})
+    all_feedbacks = Response.objects.all().order_by('-id')
+    paginator = Paginator(all_feedbacks, 20)
+    page = request.GET.get('page')
+    try:
+        all_feedbacks = paginator.page(page)
+    except PageNotAnInteger:
+        all_feedbacks=paginator.page(1)
+    except EmptyPage:
+        all_feedbacks = paginator.page(paginator.num_pages)
+    return render_to_response('feedbacks.html', {'all_feedbacks': all_feedbacks})
 
 def del_caller(request, callid):
 	Call.objects.filter(id=callid).update(caller='')
