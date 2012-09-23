952fa52de1d87a543c396fd63fca7d1205830171
Piyush Aggarwal <me.piyushaggarwal@gmail.com>
2012-09-24 00:38:43 +0530

    Update VoiceApplications/VoiceApplications/settings.py

--- VoiceApplications/VoiceApplications/settings.py
+++ VoiceApplications/VoiceApplications/settings.py
@@ -122,7 +122,7 @@ INSTALLED_APPS = (
     'PhonePeti',
     'Survey',
     'Feedback',
-	'GKAIdol',
+    'GKAIdol',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
